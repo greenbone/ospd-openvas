e0c15be05faa9497e14ffd43c0258297d726e667
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2014-07-23 13:53:42 +0000

    First approach for a config/install methodology.
    * setup.py: New. Not optimal yet, needs further attention to
    fully suffice LFH, the python way and other deployment aspects.
    
    * INSTALL: Added sections on how to install ospd and ho to create
    a source package.
    
    * README: The daemons can now be called directly if installed
    into a path-aware directory.
    
    
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/ospd@19945 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ setup.py
@@ -0,0 +1,119 @@
+# $Id$
+# Description:
+# Setup management for module ospd
+#
+# Authors:
+# Torsten Irlaender <torsten.irlaender@intevation.de>
+#
+# Copyright:
+# Copyright (C) 2014 Greenbone Networks GmbH
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2,
+# or, at your option, any later version as published by the Free
+# Software Foundation
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from setuptools import setup, find_packages # Always prefer setuptools over distutils
+from codecs import open # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ospd',
+
+    # Versions should comply with PEP440. For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='0.0.1',
+
+    description=('OSPD is a collection of scanner wrappers which share the, ',
+                 'same communication protocol: OSP (OpenVAS Scanner ',
+                 'Protocol)'),
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='http://www.openvas.org',
+
+    # Author
+    author='OpenVAS Development Team',
+    author_email='info@openvas.org',
+
+    # License
+    license='GPLv2+',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        # 3 - Alpha
+        # 4 - Beta
+        # 5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v2 or any later version (GPLv2+)'
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='OpenVAS OSP',
+
+    # List run-time dependencies here. These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
+    install_requires=['pexpect'],
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=[]),
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here. If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    include_package_data = True,
+    package_data={'': []},
+
+    # Scripts. Define scripts here which should be installed in the
+    # sys.prefix/bin directory. You can define an alternative place for
+    # installation by setting the --install-scripts option of setup.py
+    scripts = ['wrappers/ovaldi/ospd-ovaldi.py',
+               'wrappers/ovaldi/remote-ovaldi.sh',
+               'wrappers/ovaldi/remote-ovaldi-setup.sh',
+               'wrappers/w3af/ospd-w3af.py']
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    #entry_points={
+    #    'console_scripts': [
+    #        'sample=sample:main',
+    #    ],
+    #},
+)
