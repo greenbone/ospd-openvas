40ec2e57062a47d8c4c2c1ecba588167a4b70bb3
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2018-10-20 11:32:26 +0200

    Remove trailing whitespace.

--- tests/testTargetConvert.py
+++ tests/testTargetConvert.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
 
-""" Test suites for Target manipulations. 
+""" Test suites for Target manipulations.
 """
 
 import unittest
