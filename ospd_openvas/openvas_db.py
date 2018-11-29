e105db2a914fc878926fc00de773da1dd822a68e
Juan Jose Nicola <juan.nicola@greenbone.net>
2018-11-29 09:17:15 -0500

    Create the OpenvasDB class. Move global variables to the __init__ method.

--- ospd_openvas/openvas_db.py
+++ ospd_openvas/openvas_db.py
@@ -22,28 +22,16 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
-""" Functions to retrieve and store data from redis-based OpenVAS Scanner database. """
-
-# Needed to say that when we import ospd, we mean the package and not the
-# module in that directory.
-from __future__ import absolute_import
-from __future__ import print_function
+""" Functions to retrieve and store data from redis-based
+    OpenVAS Scanner database. """
 
 import redis
 import subprocess
 
-""" Path to the Redis socket. """
-DB_ADDRESS = ""
-
-""" Name of the namespace usage bitmap in redis. """
-DBINDEX_NAME = "GVM.__GlobalDBIndex"
-MAX_DBINDEX = 0
-DB_INDEX = 0
-REDISCONTEXT = None
-SOCKET_TIMEOUT = 60
+SOCKET_TIMEOUT = 60  # in seconds
 
 # Possible positions of nvt values in cache list.
-nvt_meta_fields = [
+NVT_META_FIELDS = [
     "NVT_FILENAME_POS",
     "NVT_REQUIRED_KEYS_POS",
     "NVT_MANDATORY_KEYS_POS",
@@ -60,219 +48,229 @@ nvt_meta_fields = [
     "NVT_FAMILY_POS",
     "NVT_NAME_POS",]
 
-def get_db_connection():
-    """ Retrieve the db address from openvassd config.
-    """
-    global DB_ADDRESS
-    try:
-        result = subprocess.check_output(['openvassd', '-s'],
-                                         stderr=subprocess.STDOUT)
-        result = result.decode('ascii')
-    except OSError:
-        # the command is not available
-        return 2
 
-    if result is None:
-        return 2
+class OpenvasDB(object):
+    """ Class to connect to redis, to perform queries, and to move
+    from a KB to another."""
 
-    path = None
-    for conf in result.split('\n'):
-        if conf.find("db_address") == 0:
-            path = conf.split('=')
-            break
+    def __init__(self):
+        # Path to the Redis socket.
+        self.db_address = ""
 
-    if path is None:
-        return 2
+        # Name of the namespace usage bitmap in redis.
+        self.dbindex_name = "GVM.__GlobalDBIndex"
+        self.max_dbindex = 0
+        self.db_index = 0
+        self.rediscontext = None
 
-    DB_ADDRESS = str.strip(path[1])
+    def get_db_connection(self):
+        """ Retrieve the db address from openvassd config.
+        """
+        try:
+            result = subprocess.check_output(['openvassd', '-s'],
+                                             stderr=subprocess.STDOUT)
+            result = result.decode('ascii')
+        except OSError:
+            # the command is not available
+            return 2
+
+        if result is None:
+            return 2
+
+        path = None
+        for conf in result.split('\n'):
+            if conf.find("db_address") == 0:
+                path = conf.split('=')
+                break
+
+        if path is None:
+            return 2
+
+        self.db_address = str.strip(path[1])
 
 
-def max_db_index():
-    """Set the number of databases have been configured into kbr struct.
-    """
-    global MAX_DBINDEX
-    try:
-        ctx = kb_connect()
-        resp = ctx.config_get("databases")
-    except redis.RedisError:
-        return 2
+    def max_db_index(self):
+        """Set the number of databases have been configured into kbr struct.
+        """
+        try:
+            ctx = self.kb_connect()
+            resp = ctx.config_get("databases")
+        except redis.RedisError:
+            return 2
 
-    if isinstance(resp, dict) is False:
-        return 2
-    if len(resp) == 1:
-        MAX_DBINDEX = int(resp["databases"])
-    else:
-        print("Redis: unexpected reply length %d" % len(resp))
-        return 2
+        if isinstance(resp, dict) is False:
+            return 2
+        if len(resp) == 1:
+            self.max_dbindex = int(resp["databases"])
+        else:
+            print("Redis: unexpected reply length %d" % len(resp))
+            return 2
 
-def set_global_redisctx(ctx):
-    """ Set the global set the global REDISCONTEXT.
-    """
-    global REDISCONTEXT
-    REDISCONTEXT = ctx
+    def set_redisctx(self, ctx):
+        """ Set the current rediscontext.
+        """
+        self.rediscontext = ctx
 
-def db_init():
-    """ Set DB_ADDRESS and max_db_index. """
-    if get_db_connection() or max_db_index():
-        return False
-    return True
+    def db_init(self):
+        """ Set db_address and max_db_index. """
+        if self.get_db_connection() or self.max_db_index():
+            return False
+        return True
 
-def try_database_index(ctx, i):
-    """ Check if it is already in use. If not set it as in use and return.
-    """
-    try:
-        resp = ctx.hsetnx(DBINDEX_NAME, i, 1)
-    except:
-        return 2
+    def try_database_index(self, ctx, i):
+        """ Check if it is already in use. If not set it as in use and return.
+        """
+        try:
+            resp = ctx.hsetnx(self.dbindex_name, i, 1)
+        except:
+            return 2
 
-    if isinstance(resp, int) is False:
-        return 2
+        if isinstance(resp, int) is False:
+            return 2
 
-    if resp == 1:
-        return 1
+        if resp == 1:
+            return 1
 
-def kb_connect(dbnum=0):
-    """ Connect to redis to the given database or to the default db 0 .
-    """
-    global DB_INDEX
-    if get_db_connection() is 2:
-        return 2
-    try:
-        ctx = redis.Redis(unix_socket_path=DB_ADDRESS,
-                          db=dbnum,
-                          socket_timeout=SOCKET_TIMEOUT, charset="latin-1",
-                          decode_responses=True)
-    except ConnectionError as e:
-        return {"error": str(e)}
-    DB_INDEX = dbnum
-    return ctx
+    def kb_connect(self, dbnum=0):
+        """ Connect to redis to the given database or to the default db 0 .
+        """
+        if self.get_db_connection() is 2:
+            return 2
+        try:
+            ctx = redis.Redis(unix_socket_path=self.db_address,
+                              db=dbnum,
+                              socket_timeout=SOCKET_TIMEOUT, charset="latin-1",
+                              decode_responses=True)
+        except ConnectionError as e:
+            return {"error": str(e)}
+        self.db_index = dbnum
+        return ctx
 
-def db_find(patt):
-    """ Search a pattern inside all kbs. When find it return it.
-    """
-    for i in range(0, MAX_DBINDEX):
-        ctx = kb_connect (i)
-        if ctx.keys(patt):
-            return ctx
-
-def kb_new():
-    """ Return a new kb context to an empty kb.
-    """
-    ctx = db_find(DBINDEX_NAME)
-    for index in range(1, MAX_DBINDEX):
-            if try_database_index(ctx, index) == 1:
-                ctx = kb_connect(index)
+    def db_find(self, patt):
+        """ Search a pattern inside all kbs. When find it return it.
+        """
+        for i in range(0, self.max_dbindex):
+            ctx = self.kb_connect (i)
+            if ctx.keys(patt):
                 return ctx
 
-def get_kb_context():
-    """ Get redis context if it is already connected or do a connection.
-    """
-    global REDISCONTEXT
-    if REDISCONTEXT is not None:
-        return REDISCONTEXT
+    def kb_new(self):
+        """ Return a new kb context to an empty kb.
+        """
+        ctx = self.db_find(self.dbindex_name)
+        for index in range(1, self.max_dbindex):
+                if self.try_database_index(ctx, index) == 1:
+                    ctx = self.kb_connect(index)
+                    return ctx
 
-    REDISCONTEXT = db_find(DBINDEX_NAME)
+    def get_kb_context(self):
+        """ Get redis context if it is already connected or do a connection.
+        """
+        if self.rediscontext is not None:
+            return self.rediscontext
 
-    if REDISCONTEXT is None:
-        print("Problem retrieving Redis Context")
-        return 2
+        self.rediscontext = self.db_find(self.dbindex_name)
 
-    return REDISCONTEXT
+        if self.rediscontext is None:
+            print("Problem retrieving Redis Context")
+            return 2
 
-def item_get_list(name):
-    """ Get all values under a KB key list.
-    The right global REDISCONTEXT must be already set.
-    """
-    ctx = get_kb_context()
-    return ctx.lrange(name, 0, -1)
+        return self.rediscontext
 
-def remove_list_item(key, value):
-    """ Remove item from the key list.
-    The right global REDISCONTEXT must be already set.
-    """
-    ctx = get_kb_context()
-    ctx.lrem(key, 0, value)
+    def item_get_list(self, name):
+        """ Get all values under a KB key list.
+        The right rediscontext must be already set.
+        """
+        ctx = self.get_kb_context()
+        return ctx.lrange(name, 0, -1)
 
-def item_get_single(name):
-    """ Get a single KB element. The right global REDISCONTEXT must be
-    already set.
-    """
-    ctx = get_kb_context()
-    return ctx.lindex(name, 0)
+    def remove_list_item(self, key, value):
+        """ Remove item from the key list.
+        The right rediscontext must be already set.
+        """
+        ctx = self.get_kb_context()
+        ctx.lrem(key, 0, value)
 
-def item_add_single(name, values):
-    """ Add a single KB element with one or more values.
-    The right global REDISCONTEXT must be already set.
-    """
-    ctx = get_kb_context()
-    ctx.rpush(name, *set(values))
+    def item_get_single(self, name):
+        """ Get a single KB element. The right rediscontext must be
+        already set.
+        """
+        ctx = self.get_kb_context()
+        return ctx.lindex(name, 0)
 
-def item_set_single(name, value):
-    """ Set (replace) a new single KB element. The right global
-    REDISCONTEXT must be already set.
-    """
-    ctx = get_kb_context()
-    pipe = ctx.pipeline()
-    pipe.delete(name)
-    pipe.rpush(name, *set(value))
-    pipe.execute()
+    def item_add_single(self, name, values):
+        """ Add a single KB element with one or more values.
+        The right rediscontext must be already set.
+        """
+        ctx = self.get_kb_context()
+        ctx.rpush(name, *set(values))
 
-def item_del_single(name):
-    """ Delete a single KB element. The right global REDISCONTEXT must be
-    already set.
-    """
-    ctx = get_kb_context()
-    ctx.delete(name)
+    def item_set_single(self, name, value):
+        """ Set (replace) a new single KB element. The right
+        rediscontext must be already set.
+        """
+        ctx = self.get_kb_context()
+        pipe = ctx.pipeline()
+        pipe.delete(name)
+        pipe.rpush(name, *set(value))
+        pipe.execute()
 
-def get_pattern(pattern):
-    """ Get all items stored under a given pattern.
-    """
-    ctx = get_kb_context()
-    items = ctx.keys(pattern)
+    def item_del_single(self, name):
+        """ Delete a single KB element. The right rediscontext must be
+        already set.
+        """
+        ctx = self.get_kb_context()
+        ctx.delete(name)
 
-    elem_list = []
-    for item in items:
-        elem_list.append([item, ctx.lrange(item, 0, -1)])
-    return elem_list
+    def get_pattern(self, pattern):
+        """ Get all items stored under a given pattern.
+        """
+        ctx = self.get_kb_context()
+        items = ctx.keys(pattern)
 
-def get_elem_pattern_by_index(pattern, index=1):
-    """ Get all items with index 'index', stored under
-    a given pattern.
-    """
-    ctx = get_kb_context()
-    items = ctx.keys(pattern)
+        elem_list = []
+        for item in items:
+            elem_list.append([item, ctx.lrange(item, 0, -1)])
+        return elem_list
 
-    elem_list = []
-    for item in items:
-        elem_list.append([item, ctx.lindex(item, index)])
-    return elem_list
+    def get_elem_pattern_by_index(self, pattern, index=1):
+        """ Get all items with index 'index', stored under
+        a given pattern.
+        """
+        ctx = self.get_kb_context()
+        items = ctx.keys(pattern)
 
-def release_db(kbindex=0):
-    """ Connect to redis and select the db by index.
-    Flush db and delete the index from DBINDEX_NAME list.
-    """
-    if kbindex:
-        ctx = kb_connect(kbindex)
-        ctx.flushdb()
-        ctx = kb_connect()
-        ctx.hdel(DBINDEX_NAME, kbindex)
+        elem_list = []
+        for item in items:
+            elem_list.append([item, ctx.lindex(item, index)])
+        return elem_list
 
-def get_result():
-    """ Get and remove the oldest result from the list. """
-    ctx = get_kb_context()
-    return ctx.rpop("internal/results")
+    def release_db(self, kbindex=0):
+        """ Connect to redis and select the db by index.
+        Flush db and delete the index from dbindex_name list.
+        """
+        if kbindex:
+            ctx = self.kb_connect(kbindex)
+            ctx.flushdb()
+            ctx = self.kb_connect()
+            ctx.hdel(self.dbindex_name, kbindex)
 
-def get_status():
-    """ Get and remove the oldest host scan status from the list. """
-    ctx = get_kb_context()
-    return ctx.rpop("internal/status")
+    def get_result(self):
+        """ Get and remove the oldest result from the list. """
+        ctx = self.get_kb_context()
+        return ctx.rpop("internal/results")
 
-def get_host_scan_scan_start_time():
-    """ Get the timestamp of the scan start from redis. """
-    ctx = get_kb_context()
-    return ctx.rpop("internal/start_time")
+    def get_status(self):
+        """ Get and remove the oldest host scan status from the list. """
+        ctx = self.get_kb_context()
+        return ctx.rpop("internal/status")
 
-def get_host_scan_scan_end_time():
-    """ Get the timestamp of the scan end from redis. """
-    ctx = get_kb_context()
-    return ctx.rpop("internal/end_time")
+    def get_host_scan_scan_start_time(self):
+        """ Get the timestamp of the scan start from redis. """
+        ctx = self.get_kb_context()
+        return ctx.rpop("internal/start_time")
+
+    def get_host_scan_scan_end_time(self):
+        """ Get the timestamp of the scan end from redis. """
+        ctx = self.get_kb_context()
+        return ctx.rpop("internal/end_time")
