54ca59756373dc9e7bcebbfcadd66f9e2ab82fbe
Juan Jose Nicola <juan.nicola@greenbone.net>
2019-12-05 16:04:18 +0100

    Set self.vts to None when there is a pending feed.

--- ospd_openvas/daemon.py
+++ ospd_openvas/daemon.py
@@ -398,7 +398,7 @@ class OSPDopenvas(OSPDaemon):
                     'update will be performed later.'
                 )
         elif not _running_scan and _pending_feed:
-            self.vts = dict()
+            self.vts = None
             self.load_vts()
 
     def scheduler(self):
