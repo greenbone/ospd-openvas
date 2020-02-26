37ddbaa1852d435666f5b1a460d78dd71de766c2
ArnoStiefvater <arno.stiefvater@greenbone.net>
2020-02-26 21:15:48 +0100

    update function call

--- ospd_openvas/daemon.py
+++ ospd_openvas/daemon.py
@@ -1569,10 +1569,8 @@ class OSPDopenvas(OSPDaemon):
             # Set alive test option. Overwrite the scan config settings.
             target_options = self.get_scan_target_options(scan_id)
             if target_options:
-                # check if test_alive_hosts_only() feature is active
-                # TODO: put this in build_alive_test_opt_as_prefs() and change function
-                # to nonstatic for self.openvas_db.add_item()?
-                # Or put it in completely new function? or something else?
+                # Check if test_alive_hosts_only feature of openvas is active.
+                # If active, put ALIVE_TEST enum in preferences.
                 settings = Openvas.get_settings()
                 if settings:
                     test_alive_hosts_only = settings.get(
@@ -1590,12 +1588,12 @@ class OSPDopenvas(OSPDaemon):
                                     'Invalid alive test value %s',
                                     target_options.get('alive_test'),
                                 )
-                            # put ALIVE_TEST enum in db, this is then taken by openvas to determine the method to use
+                            # Put ALIVE_TEST enum in db, this is then taken by openvas
+                            # to determine the method to use for the alive test.
                             if alive_test >= 1 and alive_test <= 31:
                                 item = 'ALIVE_TEST|||%s' % str(alive_test)
-                                self.openvas_db.add_single_item(
-                                    'internal/%s/scanprefs' % openvas_scan_id,
-                                    [item],
+                                kbdb.add_scan_preferences(
+                                    openvas_scan_id, [item]
                                 )
 
                 alive_test_opt = self.build_alive_test_opt_as_prefs(
