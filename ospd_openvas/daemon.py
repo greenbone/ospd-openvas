52b8ffdfbcc6fb9b123aabbf67386c630743295f
Juan Jose Nicola <juan.nicola@greenbone.net>
2020-02-26 15:04:47 +0100

    Use an iterator to yield the VTs directly from redis.
    The iterator uses get_single_vt() to get each vt from redis.
    It returns a tuple(vt_id, vt_values_dict), as expected for ospd.
    
    Improve readability of load_vts(), spliting and using the created get_single_vt()
    Also, load_vts() loads now the necessary to run a scan, making the vt cache smaller.

--- ospd_openvas/daemon.py
+++ ospd_openvas/daemon.py
@@ -401,95 +401,138 @@ class OSPDopenvas(OSPDaemon):
         """This method is called periodically to run tasks."""
         self.check_feed()
 
+    def get_single_vt(self, vt_id, oids):
+        _vt_params = self.nvti.get_nvt_params(vt_id)
+        _vt_refs = self.nvti.get_nvt_refs(vt_id)
+        _custom = self.nvti.get_nvt_metadata(vt_id)
+
+        if _vt_params is None or _custom is None:
+            logger.warning('Error loading VTs in memory. Trying again...')
+            return
+
+        _name = _custom.pop('name')
+        _vt_creation_time = _custom.pop('creation_date')
+        _vt_modification_time = _custom.pop('last_modification')
+
+        _vt_dependencies = list()
+        if 'dependencies' in _custom:
+            _deps = _custom.pop('dependencies')
+            _deps_list = _deps.split(', ')
+            for dep in _deps_list:
+                _vt_dependencies.append(oids.get('filename:' + dep))
+
+        _summary = None
+        _impact = None
+        _affected = None
+        _insight = None
+        _solution = None
+        _solution_t = None
+        _vuldetect = None
+        _qod_t = None
+        _qod_v = None
+
+        if 'summary' in _custom:
+            _summary = _custom.pop('summary')
+        if 'impact' in _custom:
+            _impact = _custom.pop('impact')
+        if 'affected' in _custom:
+            _affected = _custom.pop('affected')
+        if 'insight' in _custom:
+            _insight = _custom.pop('insight')
+        if 'solution' in _custom:
+            _solution = _custom.pop('solution')
+            if 'solution_type' in _custom:
+                _solution_t = _custom.pop('solution_type')
+
+        if 'vuldetect' in _custom:
+            _vuldetect = _custom.pop('vuldetect')
+        if 'qod_type' in _custom:
+            _qod_t = _custom.pop('qod_type')
+        elif 'qod' in _custom:
+            _qod_v = _custom.pop('qod')
+
+        _severity = dict()
+        if 'severity_base_vector' in _custom:
+            _severity_vector = _custom.pop('severity_base_vector')
+        else:
+            _severity_vector = _custom.pop('cvss_base_vector')
+        _severity['severity_base_vector'] = _severity_vector
+        if 'severity_type' in _custom:
+            _severity_type = _custom.pop('severity_type')
+        else:
+            _severity_type = 'cvss_base_v2'
+        _severity['severity_type'] = _severity_type
+        if 'severity_origin' in _custom:
+            _severity['severity_origin'] = _custom.pop('severity_origin')
+
+        if _name is None:
+            name = ''
+
+        vt = {'id': vt_id, 'name': _name}
+        if _custom is not None:
+            vt["custom"] = _custom
+        if _vt_params is not None:
+            vt["vt_params"] = _vt_params
+        if _vt_refs is not None:
+            vt["vt_refs"] = _vt_refs
+        if _vt_dependencies is not None:
+            vt["vt_dependencies"] = _vt_dependencies
+        if _vt_creation_time is not None:
+            vt["creation_time"] = _vt_creation_time
+        if _vt_modification_time is not None:
+            vt["modification_time"] = _vt_modification_time
+        if _summary is not None:
+            vt["summary"] = _summary
+        if _impact is not None:
+            vt["impact"] = _impact
+        if _affected is not None:
+            vt["affected"] = _affected
+        if _insight is not None:
+            vt["insight"] = _insight
+
+        if _solution is not None:
+            vt["solution"] = _solution
+            if _solution_t is not None:
+                vt["solution_type"] = _solution_t
+
+        if _vuldetect is not None:
+            vt["detection"] = _vuldetect
+
+        if _qod_t is not None:
+            vt["qod_type"] = _qod_t
+        elif _qod_v is not None:
+            vt["qod"] = _qod_v
+
+        if _severity is not None:
+            vt["severities"] = _severity
+
+        return vt
+
+    def get_vt_iterator(self):
+        """ Yield the vts from the Redis NVTicache. """
+        oids = dict(self.nvti.get_oids())
+        for filename, vt_id in oids.items():
+            vt = self.get_single_vt(vt_id, oids)
+            yield (vt_id, vt)
+
     def load_vts(self):
         """ Load the NVT's metadata into the vts
         global  dictionary. """
         logger.debug('Loading vts in memory.')
         oids = dict(self.nvti.get_oids())
-        for _filename, vt_id in oids.items():
-            _vt_params = self.nvti.get_nvt_params(vt_id)
-            _vt_refs = self.nvti.get_nvt_refs(vt_id)
-            _custom = self.nvti.get_nvt_metadata(vt_id)
-
-            if _vt_params is None or _custom is None:
-                logger.warning('Error loading VTs in memory. Trying again...')
-                return
-
-            _name = _custom.pop('name')
-            _vt_creation_time = _custom.pop('creation_date')
-            _vt_modification_time = _custom.pop('last_modification')
-
-            _summary = None
-            _impact = None
-            _affected = None
-            _insight = None
-            _solution = None
-            _solution_t = None
-            _vuldetect = None
-            _qod_t = None
-            _qod_v = None
-
-            if 'summary' in _custom:
-                _summary = _custom.pop('summary')
-            if 'impact' in _custom:
-                _impact = _custom.pop('impact')
-            if 'affected' in _custom:
-                _affected = _custom.pop('affected')
-            if 'insight' in _custom:
-                _insight = _custom.pop('insight')
-            if 'solution' in _custom:
-                _solution = _custom.pop('solution')
-                if 'solution_type' in _custom:
-                    _solution_t = _custom.pop('solution_type')
-
-            if 'vuldetect' in _custom:
-                _vuldetect = _custom.pop('vuldetect')
-            if 'qod_type' in _custom:
-                _qod_t = _custom.pop('qod_type')
-            elif 'qod' in _custom:
-                _qod_v = _custom.pop('qod')
-
-            _severity = dict()
-            if 'severity_base_vector' in _custom:
-                _severity_vector = _custom.pop('severity_base_vector')
-            else:
-                _severity_vector = _custom.pop('cvss_base_vector')
-            _severity['severity_base_vector'] = _severity_vector
-            if 'severity_type' in _custom:
-                _severity_type = _custom.pop('severity_type')
-            else:
-                _severity_type = 'cvss_base_v2'
-            _severity['severity_type'] = _severity_type
-            if 'severity_origin' in _custom:
-                _severity['severity_origin'] = _custom.pop('severity_origin')
-
-            _vt_dependencies = list()
-            if 'dependencies' in _custom:
-                _deps = _custom.pop('dependencies')
-                _deps_list = _deps.split(', ')
-                for dep in _deps_list:
-                    _vt_dependencies.append(oids.get('filename:' + dep))
-
+        for filename, vt_id in oids.items():
+            vt = self.get_single_vt(vt_id, oids)
+            custom = {'family': vt['custom'].get('family')}
             try:
                 self.add_vt(
                     vt_id,
-                    name=_name,
-                    vt_params=_vt_params,
-                    vt_refs=_vt_refs,
-                    custom=_custom,
-                    vt_creation_time=_vt_creation_time,
-                    vt_modification_time=_vt_modification_time,
-                    vt_dependencies=_vt_dependencies,
-                    summary=_summary,
-                    impact=_impact,
-                    affected=_affected,
-                    insight=_insight,
-                    solution=_solution,
-                    solution_t=_solution_t,
-                    detection=_vuldetect,
-                    qod_t=_qod_t,
-                    qod_v=_qod_v,
-                    severities=_severity,
+                    name=vt.get('name'),
+                    qod_t=vt.get('qod_type'),
+                    qod_v=vt.get('qod'),
+                    severities=vt.get('severities'),
+                    vt_modification_time=vt.get('modification_time'),
+                    vt_params=vt.get('vt_params'),
+                    custom=custom,
                 )
             except OspdError as e:
                 logger.info("Error while adding vt. %s", e)
