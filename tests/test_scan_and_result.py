2ec18e64f9f6603103f1a5f8d4f2ce64ecadc2af
Juan Jose Nicola <juan.nicola@greenbone.net>
2020-03-12 11:43:37 +0100

    Improve test Avoid a race condition, when the status is not changed but it has already an end_time Remove the assert, since this is not supposed to be tested by this test.

--- tests/test_scan_and_result.py
+++ tests/test_scan_and_result.py
@@ -508,11 +508,10 @@ class ScanTestCase(unittest.TestCase):
             scan = scans[0]
             status = scan.get('status')
 
-            if status == "init" or status == "running":
-                self.assertEqual('0', scan.get('end_time'))
-                time.sleep(0.010)
-            else:
+            if scan.get('end_time') != '0':
                 finished = True
+            else:
+                time.sleep(0.01)
 
             fs = FakeStream()
             daemon.handle_command(
