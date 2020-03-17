4f9bade56d3f44711b10a9cc73122760cddcafc8
Juan Jose Nicola <juan.nicola@greenbone.net>
2020-03-17 09:01:47 +0100

    Create PreferenceHandler class

---
+++ ospd_openvas/preferencehandler.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+# pylint: disable=too-many-lines
+
+""" Prepare the preferences to be used by OpenVAS """
+
+import uuid
+
+from typing import Optional, Dict, List, Tuple, Iterator
+
+
+class PreferenceHandler():
+
+    def __init__(self, scan_id, kbdb, scan_collection):
+        self.scan_id = scan_id
+        self.kbdb = kbdb
+        self.scan_collection = scan_collection
+        
+        self.openvas_scan_id = None
