595290fe21e302a196043ca10fc539abd4af42ba
ArnoStiefvater <arno.stiefvater@greenbone.net>
2020-07-17 14:52:11 +0200

    Fix tests

--- tests/test_daemon.py
+++ tests/test_daemon.py
@@ -42,6 +42,7 @@ OSPD_PARAMS_OUT = {
         'name': 'auto_enable_dependencies',
         'default': 1,
         'mandatory': 1,
+        'visible_for_client': 1,
         'description': 'Automatically enable the plugins that are depended on',
     },
     'cgi_path': {
@@ -49,6 +50,7 @@ OSPD_PARAMS_OUT = {
         'name': 'cgi_path',
         'default': '/cgi-bin:/scripts',
         'mandatory': 1,
+        'visible_for_client': 1,
         'description': 'Look for default CGIs in /cgi-bin and /scripts',
     },
     'checks_read_timeout': {
@@ -56,21 +58,16 @@ OSPD_PARAMS_OUT = {
         'name': 'checks_read_timeout',
         'default': 5,
         'mandatory': 1,
+        'visible_for_client': 1,
         'description': 'Number  of seconds that the security checks will '
         'wait for when doing a recv()',
-    },
-    'drop_privileges': {
-        'type': 'boolean',
-        'name': 'drop_privileges',
-        'default': 0,
-        'mandatory': 1,
-        'description': '',
     },
     'non_simult_ports': {
         'type': 'string',
         'name': 'non_simult_ports',
         'default': '22',
         'mandatory': 1,
+        'visible_for_client': 1,
         'description': 'Prevent to make two connections on the same given '
         'ports at the same time.',
     },
@@ -79,6 +76,7 @@ OSPD_PARAMS_OUT = {
         'name': 'open_sock_max_attempts',
         'default': 5,
         'mandatory': 0,
+        'visible_for_client': 1,
         'description': 'Number of unsuccessful retries to open the socket '
         'before to set the port as closed.',
     },
@@ -87,6 +85,7 @@ OSPD_PARAMS_OUT = {
         'name': 'timeout_retry',
         'default': 5,
         'mandatory': 0,
+        'visible_for_client': 1,
         'description': 'Number of retries when a socket connection attempt '
         'timesout.',
     },
@@ -95,6 +94,7 @@ OSPD_PARAMS_OUT = {
         'name': 'optimize_test',
         'default': 5,
         'mandatory': 0,
+        'visible_for_client': 1,
         'description': 'By default, openvas does not trust the remote '
         'host banners.',
     },
@@ -103,6 +103,7 @@ OSPD_PARAMS_OUT = {
         'name': 'plugins_timeout',
         'default': 5,
         'mandatory': 0,
+        'visible_for_client': 1,
         'description': 'This is the maximum lifetime, in seconds of a plugin.',
     },
     'report_host_details': {
@@ -110,6 +111,7 @@ OSPD_PARAMS_OUT = {
         'name': 'report_host_details',
         'default': 1,
         'mandatory': 1,
+        'visible_for_client': 1,
         'description': '',
     },
     'safe_checks': {
@@ -117,6 +119,7 @@ OSPD_PARAMS_OUT = {
         'name': 'safe_checks',
         'default': 1,
         'mandatory': 1,
+        'visible_for_client': 1,
         'description': 'Disable the plugins with potential to crash '
         'the remote services',
     },
@@ -125,6 +128,7 @@ OSPD_PARAMS_OUT = {
         'name': 'scanner_plugins_timeout',
         'default': 36000,
         'mandatory': 1,
+        'visible_for_client': 1,
         'description': 'Like plugins_timeout, but for ACT_SCANNER plugins.',
     },
     'time_between_request': {
@@ -132,6 +136,7 @@ OSPD_PARAMS_OUT = {
         'name': 'time_between_request',
         'default': 0,
         'mandatory': 0,
+        'visible_for_client': 1,
         'description': 'Allow to set a wait time between two actions '
         '(open, send, close).',
     },
@@ -140,6 +145,7 @@ OSPD_PARAMS_OUT = {
         'name': 'unscanned_closed',
         'default': 1,
         'mandatory': 1,
+        'visible_for_client': 1,
         'description': '',
     },
     'unscanned_closed_udp': {
@@ -147,6 +153,7 @@ OSPD_PARAMS_OUT = {
         'name': 'unscanned_closed_udp',
         'default': 1,
         'mandatory': 1,
+        'visible_for_client': 1,
         'description': '',
     },
     'expand_vhosts': {
@@ -154,6 +161,7 @@ OSPD_PARAMS_OUT = {
         'name': 'expand_vhosts',
         'default': 1,
         'mandatory': 0,
+        'visible_for_client': 1,
         'description': 'Whether to expand the target hosts '
         + 'list of vhosts with values gathered from sources '
         + 'such as reverse-lookup queries and VT checks '
@@ -164,9 +172,135 @@ OSPD_PARAMS_OUT = {
         'name': 'test_empty_vhost',
         'default': 0,
         'mandatory': 0,
+        'visible_for_client': 1,
         'description': 'If  set  to  yes, the scanner will '
         + 'also test the target by using empty vhost value '
         + 'in addition to the targets associated vhost values.',
+    },
+    'max_hosts': {
+        'type': 'integer',
+        'name': 'max_hosts',
+        'default': 30,
+        'mandatory': 0,
+        'visible_for_client': 0,
+        'description': (
+            'Is maximum number of hosts to test at the same time which'
+            + 'should be given to the client (which can override it).'
+            + 'This value must be computed given  your bandwidth,'
+            + 'the number of hosts you want to test, your amount of'
+            + 'memory and the horsepower of your processor(s).'
+        ),
+    },
+    'max_checks': {
+        'type': 'integer',
+        'name': 'max_checks',
+        'default': 10,
+        'mandatory': 0,
+        'visible_for_client': 0,
+        'description': (
+            'is the number of plugins that will run against each host being'
+            + ' tested. Note that the total number of process will be max'
+            + ' checks x max_hosts so you need to find a balance between'
+            + ' these two options. Note that launching too many plugins at'
+            + ' the same time may disable the remote host, either temporarily'
+            + ' (ie: inetd closes its ports) or definitely (the remote host'
+            + ' crash because it is asked to do too many things at the'
+            + ' same time), so be careful.'
+        ),
+    },
+    'port_range': {
+        'type': 'string',
+        'name': 'port_range',
+        'default': '',
+        'mandatory': 0,
+        'visible_for_client': 0,
+        'description': (
+            'This is the default range of ports that the scanner plugins will '
+            + 'probe. The syntax of this option is flexible, it can be a '
+            + 'single range ("1-1500"), several ports ("21,23,80"), several '
+            + 'ranges of ports ("1-1500,32000-33000"). Note that you can '
+            + 'specify UDP and TCP ports by prefixing each range by T or U. '
+            + 'For instance, the following range will make openvas scan UDP '
+            + ' ports 1 to 1024 and TCP ports 1 to 65535 : '
+            + '"T:1-65535,U:1-1024".'
+        ),
+    },
+    'test_alive_hosts_only': {
+        'type': 'boolean',
+        'name': 'test_alive_hosts_only',
+        'default': 0,
+        'mandatory': 0,
+        'visible_for_client': 0,
+        'description': (
+            'If this option is set, openvas will scan the target list for '
+            + 'alive hosts in a separate process while only testing those '
+            + 'hosts which are identified as alive. This boosts the scan '
+            + 'speed of target ranges with a high amount of dead hosts '
+            + 'significantly.'
+        ),
+    },
+    'source_iface': {
+        'type': 'string',
+        'name': 'source_iface',
+        'default': '',
+        'mandatory': 0,
+        'visible_for_client': 0,
+        'description': (
+            'Name of the network interface that will be used as the source '
+            + 'of connections established by OpenVAS. The scan won\'t be '
+            + 'launched if the value isn\'t authorized according to '
+            + '(sys_)ifaces_allow / (sys_)ifaces_deny if present.'
+        ),
+    },
+    'ifaces_allow': {
+        'type': 'string',
+        'name': 'ifaces_allow',
+        'default': '',
+        'mandatory': 0,
+        'visible_for_client': 0,
+        'description': (
+            'Comma-separated list of interfaces names that are authorized '
+            + 'as source_iface values.'
+        ),
+    },
+    'ifaces_deny': {
+        'type': 'string',
+        'name': 'ifaces_deny',
+        'default': '',
+        'mandatory': 0,
+        'visible_for_client': 0,
+        'description': (
+            'Comma-separated list of interfaces names that are not '
+            + 'authorized as source_iface values.'
+        ),
+    },
+    'hosts_allow': {
+        'type': 'string',
+        'name': 'hosts_allow',
+        'default': '',
+        'mandatory': 0,
+        'visible_for_client': 0,
+        'description': (
+            'Comma-separated list of the only targets that are authorized '
+            + 'to be scanned. Supports the same syntax as the list targets. '
+            + 'Both target hostnames and the address to which they resolve '
+            + 'are checked. Hostnames in hosts_allow list are not resolved '
+            + 'however.'
+        ),
+    },
+    'hosts_deny': {
+        'type': 'string',
+        'name': 'hosts_deny',
+        'default': '',
+        'mandatory': 0,
+        'visible_for_client': 0,
+        'description': (
+            'Comma-separated list of targets that are not authorized to '
+            + 'be scanned. Supports the same syntax as the list targets. '
+            + 'Both target hostnames and the address to which they resolve '
+            + 'are checked. Hostnames in hosts_deny list are not '
+            + 'resolved however.'
+        ),
     },
 }
 
