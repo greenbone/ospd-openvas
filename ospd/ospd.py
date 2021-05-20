983eff2a86bdda542c69c1db907d3f35412a6d0c
Juan Jose Nicola <juan.nicola@greenbone.net>
2021-05-20 14:51:27 +0200

    Wait a minute between scans starts.
    Currently the queue system in ospd prevents to start a new scan if there is not enough available memory. This produces that new scans are put in the queue, while the memory condition is not satisfied.
    Once some memory is released, all scans in queue are started. In the issue mentioned above, I pasted what I found in the log.
    
    This avoids ospd to start many queued scans simultaneously. So the last started scan has time enough to take some memory (launch some child processes).

--- ospd/ospd.py
+++ ospd/ospd.py
@@ -142,6 +142,7 @@ class OSPDaemon:
         self.max_scans = max_scans
         self.min_free_mem_scan_queue = min_free_mem_scan_queue
         self.max_queued_scans = max_queued_scans
+        self.last_scan_start_time = 0
 
         self.scaninfo_store_time = kwargs.get('scaninfo_store_time')
 
@@ -1385,6 +1386,7 @@ class OSPDaemon:
                 self.set_scan_status(scan_id, ScanStatus.INIT)
 
                 current_queued_scans = current_queued_scans - 1
+                self.last_scan_start_time = time.time()
                 logger.info('Starting scan %s.', scan_id)
             elif scan_is_queued and not scan_allowed:
                 return
@@ -1402,6 +1404,14 @@ class OSPDaemon:
                 'Not possible to run a new scan. Max scan limit set '
                 'to %d reached.',
                 self.max_scans,
+            )
+            return False
+
+        time_between_start_scan = time.time() - self.last_scan_start_time
+        if time_between_start_scan < 60:
+            logger.debug(
+                'Not possible to run a new scan right now, a scan have been '
+                'just started.'
             )
             return False
 
