39bcd93a379f59e9b26fe45da426dfd64e231baf
Christoph Kraemer <christoph.kraemer@greenbone.net>
2021-06-15 12:47:40 +0200

    add scan_id to log
    (cherry picked from commit 2df0f3dec661c07ff8ee0ca47e2f581ac330cd06)

--- ospd_openvas/db.py
+++ ospd_openvas/db.py
@@ -543,7 +543,9 @@ class KbDB(BaseKbDB):
         status = self._get_single_item('internal/{}'.format(scan_id))
 
         if status is None:
-            logger.info("Target set as finished, but the status is None.")
+            logger.info(
+                "%s: Target set as finished, but the status is None.", scan_id
+            )
 
         return status == 'finished' or status is None
 
