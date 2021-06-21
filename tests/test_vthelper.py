09b10e2429186857556e97aca3a0d514a96eb0a0
Jaspar Löchte <jaspar.loechte@greenbone.net>
2021-06-21 21:46:01 +0200

    Pylint vthelper, remove duplicated test
    (cherry picked from commit f8bab78a18fba7f9ce03447adeb3c2a4a06264ee)
    
    # Conflicts:
    #	tests/test_vthelper.py

--- tests/test_vthelper.py
+++ tests/test_vthelper.py
@@ -17,8 +17,6 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
-import logging
-
 from hashlib import sha256
 from unittest import TestCase
 
@@ -30,16 +28,16 @@ from ospd_openvas.vthelper import VtHelper
 
 class VtHelperTestCase(TestCase):
     def test_get_single_vt(self):
-        w = DummyDaemon()
-        vthelper = VtHelper(w.nvti)
+        dummy = DummyDaemon()
+        vthelper = VtHelper(dummy.nvti)
         res = vthelper.get_single_vt("1.3.6.1.4.1.25623.1.0.100061")
 
-        assert_called_once(w.nvti.get_nvt_metadata)
+        assert_called_once(dummy.nvti.get_nvt_metadata)
         self.assertEqual("Mantis Detection", res.get('name'))
 
     def test_calculate_vts_collection_hash_no_params(self):
-        w = DummyDaemon()
-        vthelper = VtHelper(w.nvti)
+        dummy = DummyDaemon()
+        vthelper = VtHelper(dummy.nvti)
         hash_out = vthelper.calculate_vts_collection_hash()
 
         vt_hash_str = (
@@ -55,8 +53,8 @@ class VtHelperTestCase(TestCase):
         self.assertEqual(hash_test, hash_out)
 
     def test_get_vt_iterator(self):
-        w = DummyDaemon()
-        vthelper = VtHelper(w.nvti)
+        dummy = DummyDaemon()
+        vthelper = VtHelper(dummy.nvti)
 
         vt = ["1.3.6.1.4.1.25623.1.0.100061"]
 
@@ -64,32 +62,169 @@ class VtHelperTestCase(TestCase):
             self.assertIn(key, vt)
 
     def test_get_vt_iterator_with_filter(self):
-        w = DummyDaemon()
-        vthelper = VtHelper(w.nvti)
+        dummy = DummyDaemon()
+        vthelper = VtHelper(dummy.nvti)
 
         vt = ["1.3.6.1.4.1.25623.1.0.100061"]
 
-        vtout = w.VTS["1.3.6.1.4.1.25623.1.0.100061"]
+        vtout = dummy.VTS["1.3.6.1.4.1.25623.1.0.100061"]
 
         for key, vt_dict in vthelper.get_vt_iterator(vt_selection=vt):
             self.assertIn(key, vt)
-            for key in vtout:
-                self.assertIn(key, vt_dict)
+            for key2 in vtout:
+                self.assertIn(key2, vt_dict)
 
     def test_get_vt_iterator_with_filter_no_vt(self):
-        w = DummyDaemon()
-        vthelper = VtHelper(w.nvti)
-        w.nvti.get_nvt_metadata.return_value = None
+        dummy = DummyDaemon()
+        vthelper = VtHelper(dummy.nvti)
+        dummy.nvti.get_nvt_metadata.return_value = None
         vt = ["1.3.6.1.4.1.25623.1.0.100065"]
 
         for _, values in vthelper.get_vt_iterator(vt_selection=vt):
             self.assertIs(values, None)
+<<<<<<< HEAD
+=======
 
-    def test_get_vt_iterator_with_filter_no_vt(self):
-        w = DummyDaemon()
-        vthelper = VtHelper(w.nvti)
-        w.nvti.get_nvt_metadata.return_value = None
-        vt = ["1.3.6.1.4.1.25623.1.0.100065"]
+    def test_get_single_vt_severity_cvssv3(self):
+        dummy = DummyDaemon()
+        dummy.nvti.get_nvt_metadata.return_value = {
+            'category': '3',
+            'creation_date': '1237458156',
+            'cvss_base_vector': 'AV:N/AC:L/Au:N/C:N/I:N/A:N',
+            'severity_vector': 'CVSS:3.0/AV:L/AC:H/PR:H/UI:R/S:U/C:N/I:L/A:L',
+            'severity_date': '1237458156',
+            'severity_origin': 'Greenbone',
+            'excluded_keys': 'Settings/disable_cgi_scanning',
+            'family': 'Product detection',
+            'filename': 'mantis_detect.nasl',
+            'last_modification': ('1533906565'),
+            'name': 'Mantis Detection',
+            'qod_type': 'remote_banner',
+            'required_ports': 'Services/www, 80',
+            'solution': 'some solution',
+            'solution_type': 'WillNotFix',
+            'solution_method': 'DebianAPTUpgrade',
+            'impact': 'some impact',
+            'insight': 'some insight',
+            'summary': ('some summary'),
+            'affected': 'some affection',
+            'timeout': '0',
+            'vt_params': {
+                '1': {
+                    'id': '1',
+                    'default': '',
+                    'description': 'Description',
+                    'name': 'Data length :',
+                    'type': 'entry',
+                },
+                '2': {
+                    'id': '2',
+                    'default': 'no',
+                    'description': 'Description',
+                    'name': 'Do not randomize the  order  in  which ports are scanned',  # pylint: disable=line-too-long
+                    'type': 'checkbox',
+                },
+            },
+            'refs': {
+                'bid': [''],
+                'cve': [''],
+                'xref': ['URL:http://www.mantisbt.org/'],
+            },
+        }
 
-        for _, values in vthelper.get_vt_iterator(vt_selection=vt):
-            self.assertIs(values, None)
+        vthelper = VtHelper(dummy.nvti)
+
+        res = vthelper.get_single_vt("1.3.6.1.4.1.25623.1.0.100061")
+        assert_called_once(dummy.nvti.get_nvt_metadata)
+
+        severities = res.get('severities')
+        self.assertEqual(
+            "CVSS:3.0/AV:L/AC:H/PR:H/UI:R/S:U/C:N/I:L/A:L",
+            severities.get('severity_base_vector'),
+        )
+        self.assertEqual("cvss_base_v3", severities.get('severity_type'))
+        self.assertEqual("Greenbone", severities.get('severity_origin'))
+        self.assertEqual("1237458156", severities.get('severity_date'))
+
+    def test_get_single_vt_severity_cvssv2(self):
+        dummy = DummyDaemon()
+        dummy.nvti.get_nvt_metadata.return_value = {
+            'category': '3',
+            'creation_date': '1237458156',
+            'cvss_base_vector': 'AV:N/AC:L/Au:N/C:N/I:N/A:N',
+            'excluded_keys': 'Settings/disable_cgi_scanning',
+            'family': 'Product detection',
+            'filename': 'mantis_detect.nasl',
+            'last_modification': ('1533906565'),
+            'name': 'Mantis Detection',
+            'qod_type': 'remote_banner',
+            'required_ports': 'Services/www, 80',
+            'solution': 'some solution',
+            'solution_type': 'WillNotFix',
+            'solution_method': 'DebianAPTUpgrade',
+            'impact': 'some impact',
+            'insight': 'some insight',
+            'summary': ('some summary'),
+            'affected': 'some affection',
+            'timeout': '0',
+            'vt_params': {
+                '1': {
+                    'id': '1',
+                    'default': '',
+                    'description': 'Description',
+                    'name': 'Data length :',
+                    'type': 'entry',
+                },
+                '2': {
+                    'id': '2',
+                    'default': 'no',
+                    'description': 'Description',
+                    'name': 'Do not randomize the  order  in  which ports are scanned',  # pylint: disable=line-too-long
+                    'type': 'checkbox',
+                },
+            },
+            'refs': {
+                'bid': [''],
+                'cve': [''],
+                'xref': ['URL:http://www.mantisbt.org/'],
+            },
+        }
+
+        vthelper = VtHelper(dummy.nvti)
+
+        res = vthelper.get_single_vt("1.3.6.1.4.1.25623.1.0.100061")
+        assert_called_once(dummy.nvti.get_nvt_metadata)
+
+        severities = res.get('severities')
+        self.assertEqual(
+            "AV:N/AC:L/Au:N/C:N/I:N/A:N",
+            severities.get('severity_base_vector'),
+        )
+        self.assertEqual("cvss_base_v2", severities.get('severity_type'))
+        self.assertEqual(None, severities.get('severity_origin'))
+        self.assertEqual("1237458156", severities.get('severity_date'))
+
+    def test_get_severity_score_v2(self):
+        dummy = DummyDaemon()
+        vthelper = VtHelper(dummy.nvti)
+        vtaux = {
+            'severities': {
+                'severity_type': 'cvss_base_v2',
+                'severity_base_vector': 'AV:N/AC:L/Au:N/C:P/I:N/A:N',
+            }
+        }
+
+        self.assertEqual(vthelper.get_severity_score(vtaux), 5.0)
+
+    def test_get_severity_score_v3(self):
+        dummy = DummyDaemon()
+        vthelper = VtHelper(dummy.nvti)
+        vtaux = {
+            'severities': {
+                'severity_type': 'cvss_base_v3',
+                'severity_base_vector': 'CVSS:3.0/AV:L/AC:H/PR:H/UI:R/S:U/C:N/I:L/A:L',
+            }
+        }
+
+        self.assertEqual(vthelper.get_severity_score(vtaux), 2.9)
+>>>>>>> f8bab78 (Pylint vthelper, remove duplicated test)
