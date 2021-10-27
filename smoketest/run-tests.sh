ba0ed5b705477c55e6f98270cb1dc210b9e362fb
Philipp Eder <philipp.eder@posteo.net>
2021-10-27 12:11:03 +0200

    Update smoketest/run-tests.sh
    Co-authored-by: Christoph Krämer <c.kraemer.rulle@web.de>

--- smoketest/run-tests.sh
+++ smoketest/run-tests.sh
@@ -2,7 +2,7 @@
 # Is a convenience script to start redis, ospd-openvas and execute `go test`
 set -e
 
-function shutdown {
+shutdown() {
   kill -9 $(cat /var/run/ospd/ospd.pid)
   redis-cli -s /var/run/redis/redis.sock SHUTDOWN
 }
