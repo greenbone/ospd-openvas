d9b68525962d091038903f86827d482b0d4ef2c7
Juan Jose Nicola <juan.nicola@greenbone.net>
2022-02-16 04:09:50 -0600

    Fix dead lock
    When the scan is started and immediately stopped it keeps looping and waiting for the already finished openvas process.

--- ospd_openvas/daemon.py
+++ ospd_openvas/daemon.py
@@ -973,9 +973,13 @@ class OSPDopenvas(OSPDaemon):
     @staticmethod
     def is_openvas_process_alive(openvas_process: psutil.Popen) -> bool:
 
-        if openvas_process.status() == psutil.STATUS_ZOMBIE:
-            logger.debug("Process is a Zombie, waiting for it to clean up")
-            openvas_process.wait()
+        try:
+            if openvas_process.status() == psutil.STATUS_ZOMBIE:
+                logger.debug("Process is a Zombie, waiting for it to clean up")
+                openvas_process.wait()
+        except psutil.NoSuchProcess:
+            return False
+
         return openvas_process.is_running()
 
     def stop_scan_cleanup(
@@ -1105,7 +1109,8 @@ class OSPDopenvas(OSPDaemon):
         # Release memory used for scan preferences.
         del scan_prefs
 
-        if do_not_launch or kbdb.scan_is_stopped(scan_id):
+        scan_stopped = self.get_scan_status(scan_id) == ScanStatus.STOPPED
+        if do_not_launch or kbdb.scan_is_stopped(scan_id) or scan_stopped:
             self.main_db.release_database(kbdb)
             return
 
@@ -1157,6 +1162,8 @@ class OSPDopenvas(OSPDaemon):
 
                 # clean main_db, but wait for scanner to finish.
                 while not kbdb.target_is_finished(scan_id):
+                    if not self.is_openvas_process_alive(openvas_process):
+                        break
                     logger.debug('%s: Waiting for openvas to finish', scan_id)
                     time.sleep(1)
                 self.main_db.release_database(kbdb)
