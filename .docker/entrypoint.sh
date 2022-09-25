33a25f906ca9c525b914ea1dfa5b3edbfd95b9ad
pascalholthaus <pascal.holthaus@greenbone.net>
2022-09-26 01:49:25 +0200

    Add: Set openvas and nmap caps at container start, with some compose installations docker forgets it.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Set openvas and nmap caps, with some compose installations docker forgets it.
+setcap cap_net_raw,cap_net_admin+eip /usr/local/sbin/openvas
+setcap cap_net_raw,cap_net_admin,cap_net_bind_service+eip /usr/bin/nmap
+
 exec gosu ospd-openvas "$@"
