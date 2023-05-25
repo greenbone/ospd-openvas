cbb479486a153ba3a6e98f070b01a0e4ee00439f
Christoph Krämer <c.kraemer.rulle@web.de>
2023-05-25 07:26:52 +0200

    Adjust all license header to the new format (#905)

--- smoketest/scan/init.go
+++ smoketest/scan/init.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Greenbone AG
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 package scan
 
 import "encoding/xml"
---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2014-2023 Greenbone AG
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
